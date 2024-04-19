c327a6e0dbf1372fe452c869ddfd9d71479f9aa9
raransyw <jeonnkook12@gmail.com>
2024-04-19 17:13:22 +0700

    Navigation

---
+++ app/src/main/java/com/example/maahirahnasywaelfianaputri_160421068_utsanmp/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maahirahnasywaelfianaputri_160421068_utsanmp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maahirahnasywaelfianaputri_160421068_utsanmp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
